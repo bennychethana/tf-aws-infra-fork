fd85d13b91d7852410de2c17b8fec67b865345d3
Chethana Benny <benny.c@northeastern.edu>
2024-10-30 18:16:01 -0400

    changes

--- iam.tf
+++ iam.tf
@@ -1,6 +1,6 @@
-# Create IAM role for EC2 to use CloudWatch
-resource "aws_iam_role" "cloudwatch_agent_role" {
-  name = "CloudWatchAgentServerRole"
+# Create combined IAM role for EC2 to access CloudWatch and S3
+resource "aws_iam_role" "ec2_role" {
+  name = "EC2CloudWatchS3Role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -18,84 +18,40 @@ resource "aws_iam_role" "cloudwatch_agent_role" {
 
 # Attach AWS managed policy for CloudWatch Agent
 resource "aws_iam_role_policy_attachment" "cloudwatch_agent_policy" {
-  role       = aws_iam_role.cloudwatch_agent_role.name
+  role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
-# Create IAM instance profile
-resource "aws_iam_instance_profile" "cloudwatch_agent_profile" {
-  name = "CloudWatchAgentServerProfile"
-  role = aws_iam_role.cloudwatch_agent_role.name
+# Create custom S3 policy to allow read, write, and delete on the bucket
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "S3FullAccessPolicy"
+  description = "Policy to allow EC2 full access to a specific S3 bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "${aws_s3_bucket.webapp_bucket.arn}/*"
+      }
+    ]
+  })
+  depends_on = [aws_s3_bucket.webapp_bucket]
 }
 
-# --------
-# resource "aws_iam_policy" "CSYE6225_Custome_Policy" {
-#   depends_on = [
-#     aws_s3_bucket.webapp_bucket
-#   ]
+# Attach S3 policy to role
+resource "aws_iam_role_policy_attachment" "s3_access_policy_attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+}
 
-#   name = "WebAppPolicy"
-
-#   policy = jsonencode(
-#     {
-#       "Version" : "2012-10-17",
-#       "Statement" : [
-#         {
-#           "Action" : [
-#             "s3:PutObject",
-#             "s3:GetObject",
-#             "s3:DeleteObject"
-#           ],
-#           "Effect" : "Allow",
-#           "Resource" : [
-#             "arn:aws:s3:::${aws_s3_bucket.webapp_bucket.bucket}",
-#             "arn:aws:s3:::${aws_s3_bucket.webapp_bucket.bucket}/*"
-#           ]
-#         }
-#       ]
-#     }
-#   )
-# }
-
-# resource "aws_iam_role" "CSYE6225_Role" {
-#   name = "CSYE6225_Role"
-
-#   depends_on = [
-#     aws_s3_bucket.webapp_bucket
-#   ]
-
-#   assume_role_policy = <<EOF
-#     {
-#         "Version": "2012-10-17",
-#         "Statement": [
-#             {
-#             "Action": "sts:AssumeRole",
-#             "Principal": {
-#                 "Service": "ec2.amazonaws.com"
-#             },
-#             "Effect": "Allow",
-#             "Sid": ""
-#             }
-#         ]
-#     }
-# EOF
-
-#   tags = {
-#     "Name" = "csye6225_role"
-#   }
-# }
-
-# resource "aws_iam_role_policy_attachment" "policy-attach" {
-#   role       = aws_iam_role.CSYE6225_Role.name
-#   policy_arn = aws_iam_policy.CSYE6225_Custome_Policy.arn
-# }
-
-# resource "aws_iam_role_policy_attachment" "policy-attach2" {
-#   role       = aws_iam_role.CSYE6225_Role.name
-#   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-# }
-
-# resource "aws_iam_instance_profile" "CSYE6225-profile" {
-#   name = "iam-profile"
-#   role = aws_iam_role.CSYE6225_Role.name
-# }
+# Create IAM instance profile for the combined role
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "EC2InstanceProfileWithCloudWatchAndS3Access"
+  role = aws_iam_role.ec2_role.name
+}
